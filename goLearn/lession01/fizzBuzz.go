0703333b1caf585b5307a91353d6637274da25d0
Saliya Gunawardena <sgunawardena@qdb.qa>
2023-06-16 16:31:57 +0300

    FizzBuzz Challenge

---
+++ goLearn/lession01/fizzBuzz.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+	for n := 1; n <= 20; n++{
+		switch {
+		case n % 3 == 0 && n % 5 == 0:
+			fmt.Println("fizz buzz")
+			// break
+		case n % 3 == 0:
+			fmt.Println("fizz ")
+		case n % 5 == 0:
+			fmt.Println("buzz ")
+		default:
+			fmt.Println(n)
+		}
+	}
+
+}
