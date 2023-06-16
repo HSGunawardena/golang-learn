f8b2a4678dea04dd5a9fb6f5f944724eb6aabbe8
Saliya Gunawardena <sgunawardena@qdb.qa>
2023-06-16 18:59:26 +0300

    updated

--- goLearn/lession01/fizzBuzz.go
+++ goLearn/lession01/fizzBuzz.go
@@ -5,11 +5,36 @@ import (
 )
 
 func main() {
+	// Challenge 01
 	fizzBuzz()
+
+	// Challenge 02
+	evenEnded()
+
+}
+
+func evenEnded() {
+	fmt.Println("================== Begin Challenge 02 ===================")
+
+	count := 0
+
+	for x := 1000; x <= 9999; x++ {
+		for y := x; y <= 9999; y++ {
+			num := x * y
+			numString := fmt.Sprintf("%d", num)
+			if numString[0] == numString[len(numString)-1] {
+				count++
+			}
+		}
+	}
+
+	fmt.Println(count)
+	fmt.Println("================== End Challenge 02 ===================")
 }
 
 func fizzBuzz() {
 
+	fmt.Println("================== Begin Challenge 01 ===================")
 	for n := 1; n <= 20; n++ {
 		switch {
 		case n%3 == 0 && n%5 == 0:
@@ -23,4 +48,5 @@ func fizzBuzz() {
 			fmt.Println(n)
 		}
 	}
+	fmt.Println("================== End Challenge 01 ===================")
 }
